da60fa78d7a816fb7774cf3af1832fb093ec23b0
CarlosBF <cborrellf@outlook.es>
2025-03-27 20:31:03 +0100

    Adjusted scrit to specified requirements

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ const counter = document.getElementById('contadorVisitas')
 const btnReset = document.getElementById('btnReestablecer')
 
 btnReset.addEventListener('click', () => {
-    counter.innerText = 1
-    localStorage.setItem('visits', 1)
+    counter.innerText = 0
+    localStorage.setItem('visits', 0)
 })
 
 if(localStorage.getItem('visits') === null){
     localStorage.setItem('visits', 1)
     counter.innerText = localStorage.getItem('visits')
 }else{
-    localStorage.setItem('visits', Number.parseFloat(localStorage.getItem('visits'))+1)
+    localStorage.setItem('visits', Number.parseInt(localStorage.getItem('visits'))+1)
     counter.innerText = localStorage.getItem('visits')
 }
